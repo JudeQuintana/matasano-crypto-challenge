a2e1dcb81cc2d98493120d200905ceac0e46693c
Jude Quintana <jude.quintana@gmail.com>
2014-08-14 13:42:13 -0600

    finished exercise5 set 1

---
+++ set-1/ch5.rb
@@ -0,0 +1,33 @@
+# http://cryptopals.com/sets/1/challenges/5/
+
+require 'pp'
+
+str = "Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"
+
+key = 'ICE'
+
+def fixed_xor(dec_str1, keys)
+
+  result =""
+  k = 0
+
+  for i in 0..(dec_str1.length-1)
+
+    if k > keys.length-1
+      k=0
+    end
+
+    xord = (dec_str1[i].bytes.first ^ keys[k].bytes.first)
+
+    result << sprintf('%02x', xord)
+
+    k+=1
+
+  end
+
+  result
+end
+
+pp fixed_xor(str, key)
+
