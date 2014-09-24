1fceb7928cbb8f484ea23141100e91491df231f8
Jude Quintana <jude.quintana@gmail.com>
2014-09-23 22:32:19 -0600

    refactored ch1 with a class, added rspec test

--- set-1/ch1.rb
+++ set-1/ch1.rb
@@ -1,13 +1,14 @@
 # http://cryptopals.com/sets/1/challenges/1/
 
+class Hex2Base64Digest
 
-string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+  def initialize(encoded_string)
+    @enc_str = encoded_string
 
-def hex_to_base64_digest(hexdigest)
-  [[hexdigest].pack("H*")].pack("m0")
+  end
+
+  def convert
+    [[@enc_str].pack("H*")].pack("m0")
+  end
+
 end
-
-base64 = hex_to_base64_digest(string)
-
-puts "string"
-p base64
