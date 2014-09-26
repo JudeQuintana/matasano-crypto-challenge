93d8caf60cf215b465c7d2ac455c5733d05a29a9
Jude Quintana <jude.quintana@gmail.com>
2014-09-26 12:54:54 -0600

    var name changes

--- lib/set-1/4-detect-single-byte-xor.rb
+++ lib/set-1/4-detect-single-byte-xor.rb
@@ -5,7 +5,7 @@ class DetectSingleByteXOR
   attr_reader :enc_hex_list_arr
 
   def initialize(hex_file_list,char_key_arr)
-    @enc_hex_list_arr = hex_file_list.split("\n")
+    @hex_list_arr = hex_file_list.split("\n")
     @char_key_arr = char_key_arr
   end
 
@@ -13,8 +13,8 @@ class DetectSingleByteXOR
 
     solution_list = {}
 
-    @enc_hex_list_arr.each_with_index do |enc_hex, index|
-      solution_list[index] = SingleByteXOR.new(enc_hex,@char_key_arr).decrypt_msg
+    @hex_list_arr.each_with_index do |hex, index|
+      solution_list[index] = SingleByteXOR.new(hex,@char_key_arr).decrypt_msg
     end
 
     solution_list
