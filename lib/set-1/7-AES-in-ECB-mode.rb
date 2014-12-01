42e3a0546aa35d6b90ae5093583108eaa1d53b87
Jude Quintana <jude.quintana@gmail.com>
2014-12-01 15:42:42 -0700

    added logic for iv if present

--- lib/set-1/7-AES-in-ECB-mode.rb
+++ lib/set-1/7-AES-in-ECB-mode.rb
@@ -5,15 +5,17 @@ require_relative '../modules/conversion'
 class AESinECB
   include Conversion
 
-  def initialize(base64_str, key)
+  def initialize(base64_str, key, iv = nil)
     @content = base64_to_ascii(base64_str)
     @key = key
+    @iv = iv
   end
 
   def decrypt
     cipher = OpenSSL::Cipher.new('AES-128-ECB')
     cipher.decrypt
     cipher.key = @key
+    cipher.iv = @iv if @iv
     cipher.update(@content) + cipher.final
   end
 
