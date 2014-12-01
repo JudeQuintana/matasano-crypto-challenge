eb83b2f460c8f5ec76d326678b5d08d245986b9b
Jude Quintana <jude.quintana@gmail.com>
2014-12-01 16:03:00 -0700

    added encrypt function and init defaults for ECB mode

--- lib/set-1/7-AES-in-ECB-mode.rb
+++ lib/set-1/7-AES-in-ECB-mode.rb
@@ -5,14 +5,23 @@ require_relative '../modules/conversion'
 class AESinECB
   include Conversion
 
-  def initialize(base64_str, key, iv = nil)
+  def initialize(base64_str, key, iv = nil, cipher_type = 'AES-128-ECB')
     @content = base64_to_ascii(base64_str)
     @key = key
     @iv = iv
+    @cipher_type = cipher_type
+  end
+
+  def encrypt
+    aes = OpenSSL::Cipher::Cipher.new(@cipher_type)
+    aes.encrypt
+    aes.key = key
+    aes.iv = iv if @iv
+    aes.update(@content) + aes.final
   end
 
   def decrypt
-    cipher = OpenSSL::Cipher.new('AES-128-ECB')
+    cipher = OpenSSL::Cipher.new(@cipher_type)
     cipher.decrypt
     cipher.key = @key
     cipher.iv = @iv if @iv
